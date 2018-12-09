51ce9691b2cfe092b926b9d8af066fca6fb7b9a8
Denis <>
2018-12-09 15:16:05 +0200

    Create

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gitdemo
+//
+//  Created by Denis on 09.12.2018.
+//  Copyright © 2018 Denis Vitrishko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
